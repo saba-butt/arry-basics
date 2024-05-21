7806b94ee4ccc171000d60918929ab429e9f85be
saba-butt <148738737+saba-butt@users.noreply.github.com>
2024-05-21 11:58:30 +0500

    Add files via upload

---
+++ saba.js
@@ -0,0 +1,36 @@
+// QUESTION NO 1
+var fruits = ["mango", "apple", "banana", "orange"];
+console.log(fruits);
+// QUESTION NO 2
+var number = [10, 20, 30, 40];
+console.log(number);
+// QUESTION NO 3
+var thirdFruit = fruits[3];
+console.log(thirdFruit);
+// QUESTION NO 4
+number[2] = 25;
+console.log(number);
+// QUESTION NO 5
+fruits.push("grape");
+console.log(fruits);
+// QUESTION NO 6
+var lastFruit = fruits.pop();
+console.log(lastFruit);
+// QUESTION NO 7
+var firstFruit = fruits.shift();
+console.log(firstFruit);
+// QUESTION NO 8
+fruits.unshift("kiwi");
+console.log(fruits);
+// QUESTION NO 9
+fruits.splice(1, 2);
+console.log(fruits);
+// QUESTION NO 10
+fruits.splice(2, 0, "pineapple", "pear");
+console.log(fruits);
+// QUESTION NO 11
+var citrusFruite = fruits.slice(0, 2);
+console.log(citrusFruite);
+// QUESTION NO 12
+var lastTwoFruits = fruits.slice(2);
+console.log(lastTwoFruits);
---
+++ saba.ts
@@ -0,0 +1,37 @@
+// QUESTION NO 1
+let fruits:string []=["mango","apple","banana","orange"]
+ console.log(fruits)
+ // QUESTION NO 2
+ let number:number[]=[10,20,30,40]
+ console.log(number)
+  // QUESTION NO 3
+ let thirdFruit = fruits[3]
+ console.log(thirdFruit)
+  // QUESTION NO 4
+ number[2]=25
+ console.log(number)
+  // QUESTION NO 5
+ fruits.push("grape")
+ console.log(fruits)
+  // QUESTION NO 6
+ let lastFruit = fruits.pop()
+ console.log(lastFruit)
+  // QUESTION NO 7
+ let firstFruit = fruits.shift()
+ console.log(firstFruit)
+  // QUESTION NO 8
+ fruits.unshift("kiwi")
+ console.log(fruits)
+  // QUESTION NO 9
+ fruits.splice(1,2)
+ console.log(fruits)
+  // QUESTION NO 10
+ fruits.splice(2,0,"pineapple","pear")
+ console.log(fruits)
+  // QUESTION NO 11
+let citrusFruite = fruits.slice(0,2)
+console.log(citrusFruite)
+   // QUESTION NO 12
+   let lastTwoFruits = fruits.slice(2)
+   console.log(lastTwoFruits)
+
